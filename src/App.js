dd3800f2d70a134e2bc02862f887fbe4eb62929d
Natanael <natanaeldepaulo123@gmail.com>
2022-06-24 20:51:52 -0300

    Cronometro basico

--- src/App.js
+++ src/App.js
@@ -1,25 +1,71 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react'
+import './app.css'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      timer: 0,
+      handleButtonText: 'Iniciar'
+    }
+    this.startTimer = null
+    this.start = this.start.bind(this)
+    this.clear = this.clear.bind(this)
+  }
+  start() {
+    let state = this.state
+    if (this.startTimer !== null) {
+      clearInterval(this.startTimer)
+      this.startTimer = null
+      state.handleButtonText = 'Retomar'
+    } else {
+      let state = this.state
+      this.startTimer = setInterval(() => {
+        state.timer += 0.1
+        this.setState(state)
+      }, 100)
+
+      state.handleButtonText = 'Pausar'
+    }
+
+    this.setState(state)
+  }
+
+  clear() {
+    if (this.startTimer !== null) {
+      clearInterval(this.startTimer)
+      this.startTimer = null
+    }
+    let state = this.state
+    state.timer = 0
+    state.handleButtonText = 'Iniciar'
+    this.setState(state)
+  }
+
+  render() {
+    return (
+      <>
+        <div className="container">
+          <div className="contentTimer">
+            <img
+              src={require('./assets/cronometro.png')}
+              alt="cronometro"
+              className="img"
+            />
+            <span className="timer">{this.state.timer.toFixed(1)}</span>
+          </div>
+          <div className="areaBtn">
+            <button className="btn" onClick={this.start}>
+              {this.state.handleButtonText}
+            </button>
+            <button className="btn" onClick={this.clear}>
+              Limpar
+            </button>
+          </div>
+        </div>
+      </>
+    )
+  }
 }
 
-export default App;
+export default App
